dc583682818af7999e95815c7328356345571b51
Ali Izoyev <ali200674@hotmail.com>
2026-04-25 20:23:09 -0700

    Adding More JS To Form

--- script.js
+++ script.js
@@ -1,72 +1,149 @@
-// All of this code is for the "Generate Weekly Totals button"
-const weeklyTotalsButton = document.querySelector("#get-totals");
+// All of this code is for the schedule
+const scheduleTable = document.querySelector("#schedule");
 const allTotalIDs = ["adsTotal", "mondayTotal", "tuesdayTotal",
     "wednesdayTotal", "thursdayTotal", "fridayTotal", "saturdayTotal",
     "sundayTotal", "rateTotal", "costTotal"
 ]
-const allColumnsIds = [
+const allColumnClasses = [
     "ads", "monday", "tuesday",
     "wednesday", "thursday", "friday", "saturday",
     "sunday", "rate", "cost"
 ]
+const allRowClasses = ["rowOne", "rowTwo", "rowThree", "rowFour", "rowFive"];
+const allRowColumnTotalID = ["rowOneTotal", "rowTwoTotal", "rowTotalThree", "rowTotalFour", "rowTotalFive"];
 
-// This will be for the "Generate Weekly Totals" button
-weeklyTotalsButton.addEventListener("click", () =>
+
+
+// This will be for the table. It will automatically listen for any inputs when the client types in a number
+scheduleTable.addEventListener("input", () =>
 {
     getAllTotals();
-    
 })
 
 /*
-    This method will take a single column and add up all of it's inputs.
-    It will then return that input to be later inputed in a Weekly Total row cell.
+    This is for the columns. It will add up all of the cells in the column and return the total
 */
-function calculateTotal(singleColumn)
+function calculateTotalColumn(singleColumn)
 {
+    // Get all of the cells in a column
     const column = document.querySelectorAll("." + singleColumn);
+
 
     let total = 0;
 
+    // For each cell
     for (const x of column)
     {
-        const value = parseFloat(x.value)
+        // Get the value (or text content for the last row)
+        let currentValue = x.value || x.textContent
 
+        // If it contains the "$", replace and trim it.
+        currentValue = currentValue.replace("$", "").trim();
+
+        // Parse it.
+        const value = parseFloat(currentValue)
+
+        // If it's not NaN, add it
         if (!isNaN(value))
         {
             total += value;
         }
     }
 
+    // Return it
+    return total;
+}
+
+// This is to calculate each row.
+function calculateTotalRow(singleRow)
+{
+    let total = 0;
+
+    const row = document.querySelectorAll("." + singleRow);
+
+    // Loop through the cells in each row
+    for (let i = 0; i < row.length; i++)
+    {
+        
+        // Get the value
+        const value = parseFloat(row[i].value)
+
+        // If the row is not empty
+        if (!isNaN(value))
+        {
+            // If it's the rate column, multiply instead of adding.
+            if (i == row.length - 1)
+            {
+                total *= value;
+            }
+            else
+            {
+                total += value;
+            }
+        }
+    }
+
+    // Return the total.
     return total;
 }
 
 /*
-    This method will get a cell from the Weekly Totals row and 
-    put the amount into that cell.
+    This is for the columns. It's basically the same thing, but no "$".
 */
-function displayTotal(totalCell, amount)
+function displayTotalColumn(totalCell, amount)
 {
     const totalCellId = document.querySelector("#" + totalCell);
 
     totalCellId.textContent = amount;
 }
 
+// This is for the rows only. It adds a "$" to every cost cell in each row.
+function displayTotalRow(totalCell, amount)
+{
+   const totalCellId = document.querySelector("#" + totalCell);
+
+    totalCellId.textContent = "$" + amount;
+} 
+
 /*
     This method will be used to generate all of the totals in the "Weekly Totals"
-    cells.
+    cells. It include the cells of each row and each column.
  */
 function getAllTotals()
 {
+    // This loop is for the rows.
+    for (let i = 0; i < allRowClasses.length; i++)
+    {
+        // This is for calculating the total row to row.
+        const rowAmount = calculateTotalRow(allRowClasses[i])
+        displayTotalRow(allRowColumnTotalID[i], rowAmount.toFixed(2))
+    }
+     
+
+    // This loop is for the columns
     for (let i = 0; i < allTotalIDs.length; i++)
     {
-        const amount = calculateTotal(allColumnsIds[i])
-        displayTotal(allTotalIDs[i], amount)
+        // This is for calculating the totals column to column
+        const amount = calculateTotalColumn(allColumnClasses[i])
+
+        // If it's the last two rows (rate and total), add a "$" to the number
+        // else, don't
+        if (i > allTotalIDs.length - 3)
+        {
+            displayTotalColumn(allTotalIDs[i], "$" + amount.toFixed(2))
+        }
+        else
+        {
+            displayTotalColumn(allTotalIDs[i], amount)
+        }   
     }
 }
 
+
+
 // Will be worked on later.
-const generateNewSchedule = document.querySelector("#make-new-schedule");
-generateNewSchedule.addEventListener("click", () =>
-{
-    console.log("test");
-})
+// const generateNewSchedule = document.querySelector("#make-new-schedule");
+// generateNewSchedule.addEventListener("click", () =>
+// {
+//     console.log("test");
+// })
